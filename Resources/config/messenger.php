eff6b9cea520b2c0373a80a90fcd51eba7e55aec
kingstar <admin@baks.dev>
2024-03-14 16:39:06 +0300

    Baks Development

--- Resources/config/messenger.php
+++ Resources/config/messenger.php
@@ -45,7 +45,9 @@ return static function(FrameworkConfig $framework) {
     ;
 
     $messenger->transport('failed-users-table')
-        ->dsn('%env(MESSENGER_TRANSPORT_DSN)%');
+        ->dsn('%env(MESSENGER_TRANSPORT_DSN)%')
+        ->options(['queue_name' => 'failed-users-table'])
+    ;
 
 };
 
