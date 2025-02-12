c5c6e5d52f327fb074da0fcd795a3a02073cbc9a
kingstar <admin@baks.dev>
2025-02-12 22:31:23 +0300

    Baks Development

--- Repository/Actions/UsersTableActionsChoice/UsersTableActionsChoiceRepository.php
+++ Repository/Actions/UsersTableActionsChoice/UsersTableActionsChoiceRepository.php
@@ -1,17 +1,17 @@
 <?php
 /*
- *  Copyright 2023.  Baks.dev <admin@baks.dev>
- *
+ *  Copyright 2025.  Baks.dev <admin@baks.dev>
+ *  
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
  *  in the Software without restriction, including without limitation the rights
  *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  *  copies of the Software, and to permit persons to whom the Software is furnished
  *  to do so, subject to the following conditions:
- *
+ *  
  *  The above copyright notice and this permission notice shall be included in all
  *  copies or substantial portions of the Software.
- *
+ *  
  *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
@@ -25,67 +25,130 @@ declare(strict_types=1);
 
 namespace BaksDev\Users\UsersTable\Repository\Actions\UsersTableActionsChoice;
 
-use BaksDev\Core\Doctrine\ORMQueryBuilder;
+use BaksDev\Core\Doctrine\DBALQueryBuilder;
+use BaksDev\Products\Category\Entity\CategoryProduct;
 use BaksDev\Products\Category\Type\Id\CategoryProductUid;
+use BaksDev\Users\Profile\UserProfile\Entity\UserProfile;
+use BaksDev\Users\Profile\UserProfile\Repository\UserProfileTokenStorage\UserProfileTokenStorageInterface;
 use BaksDev\Users\Profile\UserProfile\Type\Id\UserProfileUid;
 use BaksDev\Users\UsersTable\Entity\Actions\Event\UsersTableActionsEvent;
 use BaksDev\Users\UsersTable\Entity\Actions\Trans\UsersTableActionsTrans;
 use BaksDev\Users\UsersTable\Entity\Actions\UsersTableActions;
 use BaksDev\Users\UsersTable\Type\Actions\Event\UsersTableActionsEventUid;
+use Generator;
 
 final class UsersTableActionsChoiceRepository implements UsersTableActionsChoiceInterface
 {
-    private ORMQueryBuilder $ORMQueryBuilder;
+    private UserProfileUid|false $profile = false;
+
+    private CategoryProductUid|false $category = false;
 
     public function __construct(
-        ORMQueryBuilder $ORMQueryBuilder,
-    ) {
-        $this->ORMQueryBuilder = $ORMQueryBuilder;
+        private readonly DBALQueryBuilder $DBALQueryBuilder,
+        private readonly UserProfileTokenStorageInterface $UserProfileTokenStorage,
+    ) {}
+
+    public function forProfile(UserProfile|UserProfileUid|string $profile): self
+    {
+        if(empty($profile))
+        {
+            $this->profile = false;
+            return $this;
+        }
+
+        if(is_string($profile))
+        {
+            $profile = new UserProfileUid($profile);
+        }
+
+        if($profile instanceof UserProfile)
+        {
+            $profile = $profile->getId();
+        }
+
+        $this->profile = $profile;
+
+        return $this;
     }
+
+    public function forCategory(CategoryProduct|CategoryProductUid|string $category): self
+    {
+        if(empty($category))
+        {
+            $this->category = false;
+
+            return $this;
+        }
+
+        if(is_string($category))
+        {
+            $category = new CategoryProductUid($category);
+        }
+
+        if($category instanceof CategoryProduct)
+        {
+            $category = $category->getId();
+        }
+
+        $this->category = $category;
+
+        return $this;
+    }
+
 
     /**
      * Метод возвращает коллекцию идентификаторов активных процессов производства
      */
-    public function getCollection(UserProfileUid $profile, ?CategoryProductUid $category = null): ?array
+    public function getCollection(): Generator|false
     {
-        $qb = $this->ORMQueryBuilder->createQueryBuilder(self::class)->bindLocal();
 
-        $select = sprintf('new %s(actions.event, trans.name)', UsersTableActionsEventUid::class);
+        $dbal = $this->DBALQueryBuilder
+            ->createQueryBuilder(self::class)
+            ->bindLocal();
 
-        $qb->addSelect($select);
+        $dbal
+            ->from(UsersTableActions::class, 'actions')
+            ->where('actions.profile = :profile')
+            ->setParameter(
+                'profile',
+                $this->profile ?: $this->UserProfileTokenStorage->getProfile(),
+                UserProfileUid::TYPE
+            );
 
-        $qb->from(UsersTableActions::class, 'actions');
+        $dbal
+            ->join(
+                'actions',
+                UsersTableActionsEvent::class,
+                'event',
+                'event.id = actions.event'.
+                ($this->category ? ' AND event.category = :category' : '')
+            );
 
-        $qb->where('actions.profile = :profile')
-            ->setParameter('profile', $profile, UserProfileUid::TYPE);
-
-        $qb->join(
-            UsersTableActionsEvent::class,
-            'event',
-            'WITH',
-            'event.id = actions.event'
-        );
-
-
-        if($category)
+        if($this->category)
         {
-
-            $qb
-                ->andWhere('event.category = :category')
-                ->setParameter('category', $category, CategoryProductUid::TYPE);
-
+            $dbal
+                ->setParameter(
+                    key: 'category',
+                    value: $category,
+                    type: CategoryProductUid::TYPE
+                );
         }
 
-
-        $qb->leftJoin(
+        $dbal->leftJoin(
+            'actions',
             UsersTableActionsTrans::class,
             'trans',
-            'WITH',
             'trans.event = actions.event AND trans.local = :local'
         );
 
+        return $dbal
+            ->enableCache('users-table', 86400)
+            ->fetchAllHydrate(UsersTableActionsEventUid::class);
+
+
+        //$select = sprintf('new %s(actions.event, trans.name)', UsersTableActionsEventUid::class);
 
         /* Кешируем результат ORM */
-        return $qb->enableCache('users-table', 86400)->getResult();
+        //return $qb->enableCache('users-table', 86400)->getResult();
     }
 }
