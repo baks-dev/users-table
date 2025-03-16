7a67fd5f488cf96f6b16db6d995f02af5273ec64
kingstar <admin@baks.dev>
2025-03-16 16:49:01 +0300

    Baks Development

--- Security/Table/Header.php
+++ Security/Table/Header.php
@@ -58,7 +58,7 @@ final class Header implements MenuAdminInterface
     /**
      * Метод возвращает позицию, в которую располагается ссылка в секции меню
      */
-    public function getSortMenu(): int
+    public static function getSortMenu(): int
     {
         return 400;
     }
